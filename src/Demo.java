4e26ac94e1afb8f7db1d8633dfc0704dce27868a
Geezerkia <123498636+Geezerkia@users.noreply.github.com>
2023-03-24 22:39:59 +0200

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -7,6 +7,10 @@ public class Demo {
     }
 
     public static void allAnimalsResultOutput() {
-        System.out.println("\nУсього прийняло участь у змаганні:" + "\n------------------------------------\n" + Cat.catCount + " кішок\n" + Dog.dogCount + " собак\n" + "Разом: " + (Cat.catCount + Dog.dogCount) + " тварин.");
+        System.out.println("\nУсього прийняло участь у змаганні:" +
+                "\n------------------------------------\n" + Cat.getCatCounter() +
+                " кішок\n" + Dog.getDogCounter() + " собак\n" + "Разом: " + (Cat.getCatCounter() +
+                Dog.getDogCounter()) + " тварин.");
     }
 }
+
